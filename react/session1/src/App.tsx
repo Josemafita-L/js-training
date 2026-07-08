2c10b0a6560349ffb9635a5f0fee59108d147017
Josemafita-L <josemafita.l@gmail.com>
2026-07-08 21:22:00 +0530

    react/session1: complete task 2.1

--- react/session1/src/App.tsx
+++ react/session1/src/App.tsx
@@ -1,15 +1,10 @@
-import Greeting from './Greeting'
-
-// A React component is a reusable function that returns TSX to display UI.
-// React Fragments group multiple elements without adding an extra HTML element to the DOM.
-// Use a Fragment when you only need to group elements and don't need styling or attributes.
-// Use a <div> when you need styling, layout, className, id, or event handlers.
+import TsxRules from './TsxRules'
 
 function App() {
   return (
     <>
-      <h1>Hello React</h1>
-      <Greeting />
+      <h1>TSX Rules</h1>
+      <TsxRules />
     </>
   )
 }
