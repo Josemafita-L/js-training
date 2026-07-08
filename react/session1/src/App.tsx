2bbd16fac1b4d336865567d0502e370ba10cbe5e
Josemafita-L <josemafita.l@gmail.com>
2026-07-08 21:35:22 +0530

    react/session1: complete task 4.1

--- react/session1/src/App.tsx
+++ react/session1/src/App.tsx
@@ -1,16 +1,16 @@
-import Profile from './Profile'
+import ScoreCard from './ScoreCard'
 
 function App() {
   return (
     <>
-      <h1>Profile</h1>
-      <Profile />
+      <h1>Conditional Rendering</h1>
+      <ScoreCard />
     </>
   )
 }
 
 export default App
-// width="100" passes a string value.
-// width={100} passes a number.
-// Curly braces are used when passing JavaScript values such as variables,
-// numbers, booleans, objects, or expressions.
+
+// Use a ternary when you need to choose between two values or UI elements.
+// Ternary is an expression, so it can be used directly inside TSX.
+// An if statement is a statement and cannot be written directly inside TSX.
